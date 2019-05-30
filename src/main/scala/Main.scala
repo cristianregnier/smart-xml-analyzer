27083fb46ded3f05efb40b6b2e310b28216bbd83
Cristian Regnier <cristianregnier@gmail.com>
2019-05-30 20:46:06 -0300

    MatchingElement created

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 import com.typesafe.scalalogging.StrictLogging
 
+import scala.util.{Failure, Success}
+
 object Main extends App with StrictLogging {
 
   if (args.length < 3)
@@ -9,5 +11,11 @@ object Main extends App with StrictLogging {
   val target = args(1)
   val elementId = args(2)
 
+
+  SmartXMLParser.findMatchingElement(origin, target, elementId) match {
+    case Success(Some(element)) => logger.info(s"Matching element: ${element.path}")
+    case Success(None) => logger.info(s"No matching element was found for '${elementId}'")
+    case Failure(exception) => logger.error("Error finding match", exception)
+  }
 
 }
--- src/main/scala/SmartXMLParser.scala
+++ src/main/scala/SmartXMLParser.scala
@@ -1,9 +1,48 @@
-import org.jsoup.nodes.Element
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
 
-import scala.util.Try
+import scala.collection.JavaConverters._
+import scala.io.Source
+import scala.util.{Failure, Try}
 
 object SmartXMLParser {
 
-  def findMatchingElement(originFilePath: String, targetFilePath: String, elementId: String): Try[Option[Element]] = ???
+  private val matchingScore = 0.5
+
+  case class MatchingElement(element: Element, matchingAttributes: Seq[(String, String)]) {
+    def score: Double = matchingAttributes.size / element.attributes().size.toDouble
+
+    def path: String = {
+      val p = element.parents().asScala.reverse
+        .foldLeft(""){(acc, elem) => acc + elem.nodeName() concat ">"}
+      p concat element.nodeName()
+    }
+  }
+
+  def findMatchingElement(originFilePath: String, targetFilePath: String, elementId: String): Try[Option[MatchingElement]] = {
+    def readDocument(filename: String): Try[Document] =
+      Try(Jsoup.parse(Source.fromResource(filename).mkString))
+
+    def readElement(document: Document): Option[Element] = {
+      val element = document.getElementById(elementId)
+      if (element == null) None else Some(element)
+    }
+
+    def findMatches(element: Element, document: Document): Seq[MatchingElement] = {
+      document.getElementsByTag(element.nodeName())
+        .asScala.map(MatchingElement(_, Seq.empty))
+    }
+
+    readDocument(originFilePath).flatMap {
+      origin => {
+        readElement(origin) match {
+          case Some(element) => readDocument(targetFilePath).map {
+            target => findMatches(element, target).headOption // TODO
+          }
+          case None => Failure(throw new java.lang.RuntimeException(s"Element ${elementId} not found in origin file"))
+        }
+      }
+    }
+  }
 
 }
